7b181698598dd8ec806c7217d4c664e5859ff699
Rifat <mondolrobi@gmail.com>
2023-02-11 21:24:15 +0600

    js connect and event handler added to login button

---
+++ js/login.js
@@ -0,0 +1,12 @@
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    const emailField = document.getElementById("user-email");
+    const userEmail = emailField.value;
+
+    const passwordField = document.getElementById("user-password");
+    const userPassword = passwordField.value;
+
+
+
+    // console.log( userEmail, userPassword);
+})
